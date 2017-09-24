ce4dd6b254bed325a773c88651b3d3502b721469
tianyi <>
2017-09-23 21:41:56 -0500

    Add Shryans AR

--- Mapr/ARViewController.swift
+++ Mapr/ARViewController.swift
@@ -10,19 +10,78 @@ import UIKit
 import ARKit
 
 class ARViewController: UIViewController {
-    @IBOutlet weak var arView: ARSCNView!
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var mapButton: UIButton!
+    @IBOutlet weak var addCubeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        sceneView.session.run(configuration)
+        
+        // Round buttons
+        mapButton.layer.cornerRadius = 8
+        addCubeButton.layer.cornerRadius = 8
     }
     
+    func randFloat(min: Float, max: Float) -> Float{
+        return (Float(arc4random())/0xFFFFFFFF) * (max - min) + min
+    }
+    
+    @IBAction func addCube(_ sender: Any) {
+        
+        let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        
+        let cc = getCameraCoordinates(sceneView: sceneView)
+        cubeNode.position = SCNVector3(cc.x, cc.y, cc.z)
+        
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+    }
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning() 
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func addCup(_ sender: Any) {
+        let cupNode = SCNNode()
+        let cc = getCameraCoordinates(sceneView: sceneView)
+        cupNode.position = SCNVector3(cc.x, cc.y, cc.z)
+        
+        guard let virtualObjectScene = SCNScene(named: "cup.scn", inDirectory: "Models.scnassets/cup")
+            
+            else {
+                return
+        }
+        
+        let wrapperNode = SCNNode()
+        for child in virtualObjectScene.rootNode.childNodes{
+            child.geometry?.firstMaterial?.lightingModel = .physicallyBased
+            wrapperNode.addChildNode(child)
+        }
+        cupNode.addChildNode(wrapperNode)
+        sceneView.scene.rootNode.addChildNode(cupNode)
+    }
     
+    struct myCameraCoordinates{
+        var x = Float()
+        var y = Float()
+        var z = Float()
+    }
     
+    func getCameraCoordinates(sceneView: ARSCNView)->myCameraCoordinates{
+        let cameraTransform = sceneView.session.currentFrame?.camera.transform
+        let cameraCoordinates = MDLTransform(matrix: cameraTransform!)
+        
+        var cc = myCameraCoordinates()
+        cc.x = cameraCoordinates.translation.x
+        cc.y = cameraCoordinates.translation.y
+        cc.z = cameraCoordinates.translation.z
+        
+        return cc
+    }
 }
 
 
